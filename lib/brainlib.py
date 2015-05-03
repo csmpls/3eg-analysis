c4ffb5ab114e808ae9b5421ce080f3882a4f83f3
elsehow <ffff@ffffs-MacBook-Air.local>
2015-05-02 21:34:51 -0700

    a more sophisticated analysis that seems to do well on everything in our demo set

--- lib/brainlib.py
+++ lib/brainlib.py
@@ -29,25 +29,13 @@ def entropy(powerSpectrum,q):
     return S
 
 
+
+
 '''
 takes an array of power spectra and makes an evaluation for each point specified in xOutput. Note that I am not using the binning function anymore. The output is an array of evaluations for each powerspectrum at each time point.
-
-The get the average and the confidence intervals you need to process the array output as the following:
-
-a,s = avgPowerSpectrum(pspectra,xOutput)
-p = 1 #(confidence interval parameter, here 1%-99%)
-pDown = np.percentile(a,p,axis=0)
-pUp = np.percentile(a,100-p,axis=0)
-avg = np.mean(a,0)
-std = np.std(a,0)
 '''
 
-
-def getXOutput(spectrumSize, vectorSize):
-    return np.logspace(0, spectrumSize, vectorSize)
-
-
-def avgPowerSpectrum(pspectra,xOutput):
+def powerSpectraArray (pspectra,xOutput):
     '''Computes an average power spectrum
     from multiple power spectra, and returns 
     (interpolated) evaluations for xOutput values '''
@@ -64,13 +52,42 @@ def avgPowerSpectrum(pspectra,xOutput):
         x = np.arange(1,len(ps)+1)
         f = interp1d(x,ps/np.sum(ps))
         # try:
-        array[i] = f(xOutput)
+        array[i] = f(x)
         # except:
         #     array[i,0]=-1
         #     continue
         
-    index =np.argwhere(array[:,0]==-1)
+    index = np.argwhere(array[:,0]==-1)
     array = np.delete(array,index,0)
     #S = np.delete(S,index,0)
     return array#,S
 
+# take all values for now
+def getXOutput(spectrumSize, vectorSize):
+    return range(1, spectrumSize+1)
+    # return np.logspace(1, spectrumSize+1, num=vectorSize, dtype=int)
+
+'''
+
+The get the average and the confidence intervals you need to process the array output as the following:
+
+a,s = avgPowerSpectrum(pspectra,xOutput)
+p = 1 #(confidence interval parameter, here 1%-99%)
+pDown = np.percentile(a,p,axis=0)
+pUp = np.percentile(a,100-p,axis=0)
+avg = np.mean(a,0)
+std = np.std(a,0)
+'''
+
+def avgPowerSpectrum (powerSpectra, xOutput):
+    spectra = powerSpectraArray(powerSpectra,xOutput)
+    return np.mean(spectra, 0)
+
+# confidenceIntervalParameter of 1 is 1%-99% 
+def avgPercentileUp (powerSpectra, xOutput, confidenceIntervalParameter):
+    spectra = powerSpectraArray(powerSpectra,xOutput)
+    return np.percentile(spectra,100-confidenceIntervalParameter,axis=0)
+
+def avgPercentileDown (powerSpectra, xOutput, confidenceIntervalParameter):
+    spectra = powerSpectraArray(powerSpectra,xOutput)
+    return np.percentile(spectra,confidenceIntervalParameter,axis=0)
